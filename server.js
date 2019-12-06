964a7aac00def89484f30d1fef9250c2af2d2f82
mkayaaksoy <aksoymuratkaya@gmail.com>
2019-12-06 23:25:52 +0300

    fixed port

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const path = require('path')
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = new express();
- 
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
 });
- 
-app.listen(4020, () => {
-    console.log('App listening on port 4020')
+
+app.listen(process.env.PORT || 4020, () => {
+  console.log("App listening on port 4020");
 });
